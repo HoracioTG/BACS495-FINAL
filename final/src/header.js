057bc2a0fe838dd7070124c1aa2d855aa9cdd2dd
HoracioTG <thoraciog@gmail.com>
2022-04-12 16:45:43 -0600

    express hopefully and edited app

--- final/src/header.js
+++ final/src/header.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.png';
+
 
 function Header() {
     return (
         <div className='header_section'>
             <div className='title'>
                 <h1>Header</h1>
-            </div>
-            <div className='content'>
-                <p>This is my header (need navbar))</p>
-                <p>this is my logo <img src={logo} height={50} width={100} alt='my_logo'/></p>
             </div>
         </div>
     );
