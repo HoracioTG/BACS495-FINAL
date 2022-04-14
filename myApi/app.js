dcc3e19b0f1906a4f3f98420ec0b572a99681e33
Horacio Torres Galindo <thoraciog@gmail.com>
2022-04-13 18:23:06 -0600

    fixed api

--- myApi/app.js
+++ myApi/app.js
@@ -1,3 +1,4 @@
+var cors = require('cors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -5,9 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var questionsRouter = require('./routes/questions');
 
 var app = express();
-
+app.options('*', cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/questions', questionsRouter);
 
 module.exports = app;
